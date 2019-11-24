a4704b62e57996f29fee89158ccc5ab76aac522c
ronitgavaskar <ronitgavaskar@gmail.com>
2019-11-23 17:31:18 -0800

    testing get console log

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // This code creates a webhook endpoint that accepts POST, checks request is valid webhook event and parses message
 
 // Creates the endpoint for our webhook 
+// Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
     let body = req.body;
@@ -43,47 +44,34 @@ app.post('/webhook', (req, res) => {
 // Sends GET request in the hub.verify param
 
 // Adds support for GET requests to our webhook
+// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    const VERIFY_TOKEN = "networthverify"
-
-    // Parse the request body from the POST
-    let body = req.body;
+    let VERIFY_TOKEN = "networthverify"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
+
+    console.log("HELLO")
       
-    // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
       
-    });
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-
-
-
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
   });
   
 // Handles messages events
