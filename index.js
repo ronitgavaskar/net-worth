e38e00b21b9e9cf72798c149705a0c7b67d40523
ronitgavaskar <ronitgavaskar@gmail.com>
2019-11-23 17:03:20 -0800

    adding start script to package.json

--- index.js
+++ index.js
@@ -47,27 +47,40 @@ app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
     const VERIFY_TOKEN = "networthverify"
+
+    // Parse the request body from the POST
+    let body = req.body;
       
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
       
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+    // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+    //   console.log(webhook_event);
+      console.log("200");
       
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+
+
+
   });
   
 // Handles messages events
